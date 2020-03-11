dcf82fa75894a197c4048403f3549cbce27c208a
Srinivas K <srinivask.ria@gmail.com>
2020-03-11 14:46:21 +0530

    Sonarcube integration with Angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Hero } from './Hero';
+import { HEROES } from './mock-heroes';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+  hero: Hero = HEROES[0];
+}
